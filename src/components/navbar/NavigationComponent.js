79d46ecaa28c6901aa1222f74cff95926ed115de
Theis Langlands <thlan20@student.sdu.dk>
2023-01-06 16:07:01 +0100

    added links to navigation

--- src/components/navbar/NavigationComponent.js
+++ src/components/navbar/NavigationComponent.js
@@ -1,6 +1,9 @@
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
+import './NavigationComponent.css';
+import linkedIn from '../../assets/in_black.png';
+import github from '../../assets/github-mark.png';
 
 function NavigationComponent() {
   return (
@@ -14,6 +17,14 @@ function NavigationComponent() {
             <Nav.Link href="#projects">Projekter</Nav.Link>
             <Nav.Link href="#contact">Kontakt</Nav.Link>
           </Nav>
+          <div>
+          <a href="#linkedin" target="_blank">
+            <img className="links" src={linkedIn} alt='LinkedIn' width="30" height="30"/>
+          </a>
+          <a href="#github" target="_blank">
+            <img src={github} alt='LinkedIn' width="30" height="30"/>
+          </a>
+          </div>
         </Navbar.Collapse>
       </Container>
     </Navbar>
